f26f3111ba82a5bce42296587437eeda7664f54a
Alan Yu <yupliu@users.noreply.github.com>
2017-07-26 23:17:50 -0400

    phase1

--- Rg_wk2_02/Rg_wk2_02/Rg_wk2_02.py
+++ Rg_wk2_02/Rg_wk2_02/Rg_wk2_02.py
@@ -15,3 +15,16 @@ sales = graphlab.SFrame('C:\\Machine_Learning\\kc_house_data.gl\\')
 (example_features, example_output) = get_numpy_data(sales, ['sqft_living'], 'price') # the [] around 'sqft_living' makes it a list
 print example_features[0,:] # this accesses the first row of the data the ':' indicates 'all columns'
 print example_output[0] # and the corresponding output
+
+my_weights = np.ones(2)
+my_features = example_features[0,:]
+predicted_value = np.dot(my_weights,my_features)
+
+def predict_output(feature_matrix,weights):
+    pred = np.dot(feature_matrix,weights)
+    return pred
+
+test_predictions = predict_output(example_features, my_weights)
+print test_predictions[0] # should be 1181.0
+print test_predictions[1] # should be 2571.0
+
